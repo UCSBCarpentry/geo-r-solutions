9b66ee4f8ce2b71529f94b5b015d50c67badaa27
jonjab <jonjab@ucsb.edu>
2022-07-25 15:12:23 -0700

    monday morning additions

---
+++ ep2.r
@@ -0,0 +1,210 @@
+# episode 2. Geospatial R
+
+library(raster)
+library(rgdal)
+library(ggplot2)
+library(dplyr)
+
+# our existing dataframe
+HARV_DSM_df
+
+# mutate into bins
+HARV_DSM_df <- HARV_DSM_df %>%
+  mutate(fct_elevation = cut(Altitude, breaks = 3))
+
+ggplot() +
+  geom_bar(data = HARV_DSM_df, aes(fct_elevation))
+
+unique(HARV_DSM_df$fct_elevation)
+
+HARV_DSM_df %>%
+  group_by(fct_elevation) %>%
+  count()
+
+# mutate into specified bins
+custom_bins <- c(300, 350, 400, 450)
+
+HARV_DSM_df <- HARV_DSM_df %>%
+  mutate(fct_elevation_2 = cut(Altitude, breaks = custom_bins))
+
+unique(HARV_DSM_df$fct_elevation_2)
+
+# this again highlights the over 400.
+ggplot() +
+  geom_bar(data = HARV_DSM_df, aes(fct_elevation_2))
+
+# and get the count of pixels in each bin
+HARV_DSM_df %>%
+  group_by(fct_elevation_2) %>%
+  count()
+
+# map it
+ggplot() +
+  geom_raster(data = HARV_DSM_df , aes(x = x, y = y, fill = fct_elevation_2)) + 
+  coord_quickmap()
+
+# that default color scheme doesn't work so well.
+# try this one:
+terrain.colors(3)
+
+ggplot() +
+  geom_raster(data = HARV_DSM_df , aes(x = x, y = y,
+                                       fill = fct_elevation_2)) + 
+  scale_fill_manual(values = terrain.colors(3)) + 
+  coord_quickmap()
+
+# save your colors in an object for re-use
+# and add a title to the legend
+my_col <- terrain.colors(3)
+
+ggplot() +
+  geom_raster(data = HARV_DSM_df , aes(x = x, y = y,
+                                       fill = fct_elevation_2)) + 
+  scale_fill_manual(values = my_col, name = "Elevation") + 
+  coord_quickmap()
+
+# challenge plot
+HARV_DSM_df <- HARV_DSM_df  %>%
+  mutate(fct_elevation_6 = cut(Altitude, breaks = 6)) 
+
+my_col <- terrain.colors(6)
+
+ggplot() +
+  geom_raster(data = HARV_DSM_df , aes(x = x, y = y,
+                                       fill = fct_elevation_6)) + 
+  scale_fill_manual(values = my_col, name = "Elevation") + 
+  ggtitle("Classified Elevation Map - NEON Harvard Forest Field Site") +
+  xlab("UTM Easting Coordinate (m)") +
+  ylab("UTM Northing Coordinate (m)") + 
+  coord_quickmap()
+
+
+# layering rasters
+HARV_hill <-
+  raster("data/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/HARV_DSMhill.tif")
+
+HARV_hill
+HARV_hill_df <- as.data.frame(HARV_hill, xy = TRUE) 
+str(HARV_hill_df)
+
+# plot that with no legend
+ggplot() +
+  geom_raster(data = HARV_hill_df,
+              aes(x = x, y = y, alpha = HARV_DSMhill)) + 
+  scale_alpha(range =  c(0.15, 0.65), guide = "none") + 
+  coord_quickmap()
+
+# the top layer needs an alpha if you hope to see it.
+# plots build in the order you call the geom's
+ggplot() +
+  geom_raster(data = HARV_DSM_df , 
+              aes(x = x, y = y, 
+                  fill = Altitude)) + 
+  geom_raster(data = HARV_hill_df, 
+              aes(x = x, y = y, 
+                  alpha = HARV_DSMhill)) +  
+  scale_fill_viridis_c() +  
+  scale_alpha(range = c(0.15, 0.65), guide = "none") +  
+  ggtitle("Elevation with hillshade") +
+  coord_quickmap()
+
+
+###################################
+# challenge map: do it with SJER
+# CREATE DSM MAPS
+
+# import DSM data
+SJER_DSM <- raster("data/NEON-DS-Airborne-Remote-Sensing/SJER/DSM/SJER_dsmCrop.tif")
+# convert to a df for plotting
+SJER_DSM_df <- as.data.frame(SJER_DSM, xy = TRUE)
+
+# import DSM hillshade
+SJER_DSM_hill <- raster("data/NEON-DS-Airborne-Remote-Sensing/SJER/DSM/SJER_dsmHill.tif")
+# convert to a df for plotting
+SJER_DSM_hill_df <- as.data.frame(SJER_DSM_hill, xy = TRUE)
+
+# Build Plot
+ggplot() +
+  geom_raster(data = SJER_DSM_df , 
+              aes(x = x, y = y, 
+                  fill = SJER_dsmCrop,
+                  alpha = 0.8)
+  ) + 
+  geom_raster(data = SJER_DSM_hill_df, 
+              aes(x = x, y = y, 
+                  alpha = SJER_dsmHill)
+  ) +
+  scale_fill_viridis_c() +
+  guides(fill = guide_colorbar()) +
+  scale_alpha(range = c(0.4, 0.7), guide = "none") +
+  # remove grey background and grid lines
+  theme_bw() + 
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) +
+  xlab("UTM Easting Coordinate (m)") +
+  ylab("UTM Northing Coordinate (m)") +
+  ggtitle("DSM with Hillshade") +
+  coord_quickmap()
+
+
+# CREATE DTM MAP
+# import DTM
+# think: T = Treetops.
+DTM_SJER <- raster("data/NEON-DS-Airborne-Remote-Sensing/SJER/DTM/SJER_dtmCrop.tif")
+DTM_SJER_df <- as.data.frame(DTM_SJER, xy = TRUE)
+
+# DTM Hillshade
+DTM_hill_SJER <- raster("data/NEON-DS-Airborne-Remote-Sensing/SJER/DTM/SJER_dtmHill.tif")
+DTM_hill_SJER_df <- as.data.frame(DTM_hill_SJER, xy = TRUE)
+
+ggplot() +
+  geom_raster(data = DTM_SJER_df ,
+              aes(x = x, y = y,
+                  fill = SJER_dtmCrop,
+                  alpha = 2.0)
+  ) +
+  geom_raster(data = DTM_hill_SJER_df,
+              aes(x = x, y = y,
+                  alpha = SJER_dtmHill)
+  ) +
+  scale_fill_viridis_c() +
+  guides(fill = guide_colorbar()) +
+  scale_alpha(range = c(0.4, 0.7), guide = "none") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+  ggtitle("DTM with Hillshade") +
+  coord_quickmap()
+
+# CREATE DTM MAP
+# import DTM
+SJER_DTM <- raster("data/NEON-DS-Airborne-Remote-Sensing/SJER/DTM/SJER_dtmCrop.tif")
+SJER_DTM_df <- as.data.frame(SJER_DTM, xy = TRUE)
+
+# DTM Hillshade
+DTM_hill_SJER <- raster("data/NEON-DS-Airborne-Remote-Sensing/SJER/DTM/SJER_dtmHill.tif")
+DTM_hill_SJER_df <- as.data.frame(DTM_hill_SJER, xy = TRUE)
+
+ggplot() +
+  geom_raster(data = SJER_DTM_df ,
+              aes(x = x, y = y,
+                  fill = SJER_dtmCrop,
+                  alpha = 2.0)
+  ) +
+  geom_raster(data = DTM_hill_SJER_df,
+              aes(x = x, y = y,
+                  alpha = SJER_dtmHill)
+  ) +
+  scale_fill_viridis_c() +
+  guides(fill = guide_colorbar()) +
+  scale_alpha(range = c(0.4, 0.7), guide = "none") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+  ggtitle("DTM with Hillshade") +
+  coord_quickmap()
+
---
+++ ep3.r
@@ -0,0 +1,99 @@
+# helpful R command to see all the objects in memory
+ls()
+
+
+# we made a DTM for SJER. Now let's make one for HARV
+HARV_DTM <- raster("data/NEON-DS-Airborne-Remote-Sensing/HARV/DTM/HARV_dtmCrop.tif")
+HARV_DTM_hill <- raster("data/NEON-DS-Airborne-Remote-Sensing/HARV/DTM/HARV_DTMhill_WGS84.tif")
+HARV_DTM_df <- as.data.frame(HARV_DTM, xy = TRUE)
+HARV_DTM_hill_df <- as.data.frame(HARV_DTM_hill, xy = TRUE)
+
+# this one is intentionally wrong
+ggplot() +
+  geom_raster(data = HARV_DTM_df , 
+              aes(x = x, y = y, 
+                  fill = HARV_dtmCrop)) + 
+  geom_raster(data = HARV_DTM_hill_df, 
+              aes(x = x, y = y, 
+                  alpha = HARV_DTMhill_WGS84)) +
+  scale_fill_gradientn(name = "Elevation", colors = terrain.colors(10)) + 
+  coord_quickmap()
+
+# each one plots independently
+ggplot() +
+  geom_raster(data = HARV_DTM_df,
+              aes(x = x, y = y,
+                  fill = HARV_dtmCrop)) +
+  scale_fill_gradientn(name = "Elevation", colors = terrain.colors(10)) + 
+  coord_quickmap()
+
+ggplot() +
+  geom_raster(data = HARV_DTM_hill_df,
+              aes(x = x, y = y,
+                  alpha = HARV_DTMhill_WGS84)) + 
+  coord_quickmap()
+
+# we could have figured this out first
+# (read the first line)
+crs(HARV_DTM)
+crs(HARV_DTM_hill)
+
+(HARV_DTM)
+(HARV_DTM_hill)
+
+
+# project can be a copy and paste operation
+# projectRaster(RasterObject, crs = CRSToReprojectTo)
+
+HARV_DTM_hill_reprojected <- projectRaster(HARV_DTM_hill,
+                                           crs = crs(HARV_DTM))
+
+
+(HARV_DTM_hill_reprojected)
+(HARV_DTM)
+
+
+# this still doesn't work because the EXTENTS don't match
+HARV_DTM_hill_reprojected_df <- as.data.frame(HARV_DTM_hill_reprojected, xy=TRUE)
+ggplot() +
+  geom_raster(data = HARV_DTM_df , 
+              aes(x = x, y = y, 
+                  fill = HARV_dtmCrop)) + 
+  geom_raster(data = HARV_DTM_hill_df, 
+              aes(x = x, y = y, 
+                  alpha = HARV_DTMhill_WGS84)) +
+  scale_fill_gradientn(name = "Elevation", colors = terrain.colors(10)) + 
+  coord_quickmap()
+
+# you can see that
+extent(HARV_DTM_hill_reprojected)
+extent(HARV_DTM_hill)
+
+# easier to determine:
+extent(HARV_DTM_hill_reprojected) == extent(HARV_DTM_hill)
+
+# another thing that doesn't match after reprojection is pixel size
+res(HARV_DTM_hill_reprojected)
+res(HARV_DTM)
+
+# force them to match
+HARV_DTM_hill_reprojected <- projectRaster(HARV_DTM_hill,
+                                           crs = crs(HARV_DTM),
+                                           res = res(HARV_DTM)) 
+
+# make a dataframe
+HARV_DTM_hill_2_df <- as.data.frame(HARV_DTM_hill_reprojected, xy = TRUE)
+
+ggplot() +
+  geom_raster(data = HARV_DTM_df , 
+              aes(x = x, y = y, 
+                  fill = HARV_dtmCrop)) + 
+  geom_raster(data = HARV_DTM_hill_2_df, 
+              aes(x = x, y = y, 
+                  alpha = HARV_DTMhill_WGS84)) +
+  scale_fill_gradientn(name = "Elevation", colors = terrain.colors(10)) + 
+  coord_quickmap()
+
+# take home message:
+# projection, extent, and resolution all have
+# to match!
