2fdaa8f14a26b3b52be6559fa2c8ac6b311167aa
jonjab <jonjab@ucsb.edu>
2023-07-06 15:55:29 -0700

    started editing the script with new terra version of the lesson

--- ep1.r
+++ ep1.r
@@ -1,69 +1,90 @@
-library(raster)
-library(rgdal)
+# re-writing for the terra-instance of the lesson
+
+# library(raster)
+# library(rgdal)
 library(ggplot2)
 library(dplyr)
+library(terra)
 
-GDALinfo("data/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/HARV_dsmCrop.tif")
+# lesson text says we'll use sf in this episode
+# but I don't think so
 
-HARV_DSM_info <- capture.output(
-  GDALinfo("data/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/HARV_dsmCrop.tif")
+describe("data/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/DSM_HARVCrop.tif")
+
+DSM_HARV_info <- capture.output(
+  describe("data/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/DSM_HARVCrop.tif")
 )
 
-HARV_DSM <- 
-  raster("data/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/HARV_dsmCrop.tif")
 
-HARV_DSM
+DSM_HARV <- 
+  rast("data/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/DSM_HARVCrop.tif")
 
-summary(HARV_DSM)
+DSM_HARV
 
-summary(HARV_DSM, maxsamp = ncell(HARV_DSM))
+summary(DSM_HARV)
 
-HARV_DSM_df <- as.data.frame(HARV_DSM, xy = TRUE)
+summary(values(DSM_HARV))
 
-str(HARV_DSM_df)
+# or the tidy way
+values(DSM_HARV) %>% summary()
+
+# as usual, ggplot wants dataframes
+DSM_HARV_df <- as.data.frame(DSM_HARV, xy = TRUE)
+
+str(DSM_HARV_df)
 
 # change the name of the column to better reflect what it is:
+# this isn't in the lesson
+names(DSM_HARV_df)[names(DSM_HARV_df) == 'HARV_dsmCrop'] <- 'Elevation'
 
-names(HARV_DSM_df)[names(HARV_DSM_df) == 'HARV_dsmCrop'] <- 'Altitude'
+str(DSM_HARV_df)
 
 ggplot() +
-  geom_raster(data = HARV_DSM_df , aes(x = x, y = y, fill = Altitude)) +
+  geom_raster(data = DSM_HARV_df , aes(x = x, y = y, fill = Elevation)) +
   scale_fill_viridis_c() +
   coord_quickmap()
 
-crs(HARV_DSM)
+crs(DSM_HARV)
+
+################## left off here ##########
+# calculating maxes and mins
+minmax(DSM_HARV)
+
+min(values(DSM_HARV))
+max(values(DSM_HARV))
+
 
 # deal with NA's
 
 ggplot() +
-  geom_raster(data = HARV_DSM_df , aes(x = x, y = y, fill = Altitude)) +
+  geom_raster(data = DSM_HARV_df , aes(x = x, y = y, fill = Altitude)) +
   scale_fill_viridis_c(na.value = 'deeppink') +
   coord_quickmap()
 
 # no value = -9999
-HARV_DSM_info
+DSM_HARV_info
 
-summary(HARV_DSM_df)
+summary(DSM_HARV_df)
 
 # this shows our current raster doesn't have any.
-summary(HARV_DSM)
+summary(DSM_HARV)
 
 # dealing with bad values
 # lesson shows a figure that it doesn't make.
 # here's the code:
-DSM_highvals <- reclassify(HARV_DSM, rcl = c(0, 400, NA_integer_, 400, 420, 1L), include.lowest = TRUE)
+DSM_highvals <- reclassify(DSM_HARV, rcl = c(0, 400, NA_integer_, 400, 420, 1L), include.lowest = TRUE)
 # ^^^^^^^^
 # that's not very elegant at all. There must be a way to get only > 400 and overlay it.
 
 DSM_highvals <- as.data.frame(DSM_highvals, xy = TRUE)
 str(DSM_highvals)
 # change that name again
-names(DSM_highvals)[names(DSM_highvals) == 'HARV_dsmCrop'] <- 'Altitude'
+names(DSM_highvals)[names(DSM_highvals) == 'DSM_HARVCrop'] <- 'Altitude'
 
 str(DSM_highvals)
 
 ggplot() +
-  geom_raster(data = HARV_DSM_df, aes(x = x, y = y, fill = Altitude)) + 
+  geom_raster(data = DSM_HARV_df, aes(x = x, y = y, fill = Altitude)) + 
   scale_fill_viridis_c() + 
   # use reclassified raster data as an annotation
   annotate(geom = 'raster', x = DSM_highvals$x, y = DSM_highvals$y, fill = scales::colour_ramp('deeppink')(DSM_highvals$Altitude)) +
@@ -78,10 +99,10 @@ ggplot() +
 summary(DSM_highvals)
 
 ggplot() +
-  geom_histogram(data = HARV_DSM_df, aes(Altitude))
+  geom_histogram(data = DSM_HARV_df, aes(Altitude))
 
 # skipped making more bins.
 
 # Challenge
 # geting info about another raster
-GDALinfo("data/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/HARV_DSMhill.tif")
+GDALinfo("data/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/DSM_HARVhill.tif")
