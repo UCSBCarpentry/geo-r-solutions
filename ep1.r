9304dee2c32b64c76be980f40373d92190a4af9e
kristi-sara <ksliu@ucsb.edu>
2023-07-11 15:32:04 -0700

    going back into episode 1 to change naming for HARV_DSM

--- ep1.r
+++ ep1.r
@@ -12,65 +12,65 @@ library(terra)
 getwd()
 describe("data/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/HARV_dsmCrop.tif")
 
-DSM_HARV_info <- capture.output(
+HARV_DSM_info <- capture.output(
   describe("data/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/HARV_dsmCrop.tif")
 )
 
 
-DSM_HARV <- 
-  rast("data/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/HARV_dsmCrop.tif")
+HARV_DSM <- 
+  raster("data/NEON-DS-Airborne-Remote-Sensing/HARV/DSM/HARV_dsmCrop.tif")
 
-DSM_HARV
+HARV_DSM
 
-summary(DSM_HARV)
+summary(HARV_DSM)
 
-summary(values(DSM_HARV))
+summary(values(HARV_DSM))
 
 # or the tidy way
-values(DSM_HARV) %>% summary()
+values(HARV_DSM) %>% summary()
 
 # as usual, ggplot wants dataframes
-DSM_HARV_df <- as.data.frame(DSM_HARV, xy = TRUE)
+HARV_DSM_df <- as.data.frame(HARV_DSM, xy = TRUE)
 
-str(DSM_HARV_df)
+str(HARV_DSM_df)
 
 # change the name of the column to better reflect what it is:
 # this isn't in the lesson
-names(DSM_HARV_df)[names(DSM_HARV_df) == 'HARV_dsmCrop'] <- 'Elevation'
+names(HARV_DSM_df)[names(HARV_DSM_df) == 'HARV_dsmCrop'] <- 'Elevation'
 
 
-str(DSM_HARV_df)
+str(HARV_DSM_df)
 
 ggplot() +
-  geom_raster(data = DSM_HARV_df , aes(x = x, y = y, fill = Elevation)) +
+  geom_raster(data = HARV_DSM_df , aes(x = x, y = y, fill = Elevation)) +
   scale_fill_viridis_c() +
   coord_quickmap()
 
-crs(DSM_HARV)
+crs(HARV_DSM)
 
 # calculating maxes and mins
-minmax(DSM_HARV)
+minmax(HARV_DSM)
 
-min(values(DSM_HARV))
-max(values(DSM_HARV))
+min(values(HARV_DSM))
+max(values(HARV_DSM))
 
 
 # deal with NA's
 # I think this ggplot should go after any(is.na) and summary()
 ggplot() +
-  geom_raster(data = DSM_HARV_df , aes(x = x, y = y, fill = Elevation)) +
+  geom_raster(data = HARV_DSM_df , aes(x = x, y = y, fill = Elevation)) +
   scale_fill_viridis_c(na.value = 'deeppink') +
   coord_quickmap()
 
 #check if there are actually any NA's
 #A few ways: 
-any(is.na(DSM_HARV_df$Elevation))
+any(is.na(HARV_DSM_df$Elevation))
 
 # no value = -9999
-DSM_HARV_info
+HARV_DSM_info
 
 # this shows our current raster doesn't have any.
-summary(DSM_HARV)
+summary(HARV_DSM)
 
 ### To get a similar image in the lesson example
 # Load image into dataframe
@@ -87,7 +87,7 @@ ggplot() +
 # lesson shows a figure that it doesn't make.
 # here's the code:
 # The line below is throwing me off: 
-DSM_highvals <- classify(DSM_HARV, rcl = c(0, 400, NA, 420, 1L), include.lowest = TRUE)
+DSM_highvals <- classify(HARV_DSM, rcl = c(0, 400, NA, 420, 1L), include.lowest = TRUE)
 
 # ^^^^^^^^
 # that's not very elegant at all. There must be a way to get only > 400 and overlay it.
@@ -123,7 +123,7 @@ ggplot() +
 summary(DSM_highvals)
 
 ggplot() +
-  geom_histogram(data = DSM_HARV_df, aes(Elevation))
+  geom_histogram(data = HARV_DSM_df, aes(Elevation))
 
 # skipped making more bins.
 
