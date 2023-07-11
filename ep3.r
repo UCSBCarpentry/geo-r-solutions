8c150341805c835760f928dedb53611f9a8eeb68
kristi-sara <ksliu@ucsb.edu>
2023-07-11 11:56:22 -0700

    finish ep 3 challenge move to ep 4

--- ep3.r
+++ ep3.r
@@ -1,7 +1,6 @@
 # helpful R command to see all the objects in memory
 ls()
 
-#kristi starting episode 3
 # we made a DTM for SJER. Now let's make one for HARV
 HARV_DTM <- raster("data/NEON-DS-Airborne-Remote-Sensing/HARV/DTM/HARV_dtmCrop.tif")
 HARV_DTM_hill <- raster("data/NEON-DS-Airborne-Remote-Sensing/HARV/DTM/HARV_DTMhill_WGS84.tif")
@@ -98,7 +97,7 @@ ggplot() +
 # projection, extent, and resolution all have
 # to match!
 
-#kristi to here now
+
 ##############################
 # challenge: make an overlay for SJER
 # import DSM
