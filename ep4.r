73d6052799718082096a8e1f8453c3d9ea2a3d4d
kristi-sara <ksliu@ucsb.edu>
2023-07-17 15:47:44 -0700

    changed naming to match lesson. last save output giving offs, format error, but documentation says we are correct

--- ep4.r
+++ ep4.r
@@ -41,48 +41,50 @@ ggplot(CHM_HARV_df) +
   geom_histogram(aes(HARV_dsmCrop))
 
 # challenge
-min(HARV_CHM_df$layer, na.rm = TRUE)
+min(CHM_HARV_df$HARV_dsmCrop, na.rm = TRUE)
 
-ggplot(HARV_CHM_df) +
-  geom_histogram(aes(layer))
+max(CHM_HARV_df$HARV_dsmCrop, na.rm = TRUE)
 
-ggplot(HARV_CHM_df) +
-  geom_histogram(aes(layer), colour="black", 
+ggplot(CHM_HARV_df) +
+  geom_histogram(aes(HARV_dsmCrop))
+
+ggplot(CHM_HARV_df) +
+  geom_histogram(aes(HARV_dsmCrop), colour="black", 
                  fill="darkgreen", bins = 6)
 
 # the above has auto-breaks. we want to specify our own.
 custom_bins <- c(0, 10, 20, 30, 40)
-HARV_CHM_df <- HARV_CHM_df %>%
-  mutate(canopy_discrete = cut(layer, breaks = custom_bins))
+CHM_HARV_df <- CHM_HARV_df %>%
+  mutate(canopy_discrete = cut(HARV_dsmCrop,
+                               breaks = custom_bins))
 
 ggplot() +
-  geom_raster(data = HARV_CHM_df , aes(x = x, y = y,
-                                       fill = canopy_discrete)) + 
+  geom_raster(data = CHM_HARV_df, aes(x = x, y = y,
+                                  fill = canopy_discrete)) + 
   scale_fill_manual(values = terrain.colors(4)) + 
   coord_quickmap()
 
 # this is the second way to do raster math: an overlay
-HARV_CHM_ov <- overlay(HARV_DSM,
-                       HARV_DTM,
+CHM_ov_HARV <- lapp(sds(list(DSM_HARV,DTM_HARV)),
                        fun = function(r1, r2) { return( r1 - r2) })
 
-HARV_CHM_ov_df <- as.data.frame(HARV_CHM_ov, xy = TRUE)
+CHM_ov_HARV_df <- as.data.frame(CHM_ov_HARV, xy = TRUE)
 
 ggplot() +
-  geom_raster(data = HARV_CHM_ov_df, 
-              aes(x = x, y = y, fill = layer)) + 
+  geom_raster(data = CHM_ov_HARV_df, 
+              aes(x = x, y = y, fill = HARV_dsmCrop)) + 
   scale_fill_gradientn(name = "Canopy Height", colors = terrain.colors(10)) + 
   coord_quickmap()
 
 
 # you'll see these are the same.
-max(HARV_CHM_ov_df$layer, na.rm = TRUE)
-max(HARV_CHM_df$layer, na.rm = TRUE)
+max(CHM_ov_HARV_df$HARV_dsmCrop, na.rm = TRUE)
+max(CHM_HARV_df$HARV_dsmCrop, na.rm = TRUE)
 
 # save our work
 # this object name might be wrong 
 # in the lesson!
-writeRaster(HARV_CHM_ov, "HARV_CHM.tiff",
+writeRaster(CHM_ov_HARV, "CHM_HARV.tiff",
             format="GTiff",
             overwrite=TRUE,
             NAflag=-9999)
