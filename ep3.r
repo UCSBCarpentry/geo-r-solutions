ba15ce51bef8073f79907539e1f3a2599b24b49d
kristi-sara <ksliu@ucsb.edu>
2023-07-17 15:20:42 -0700

    rename variables, on line 116, lesson says DTM_hill_UTM, but it should be DSM

--- ep3.r
+++ ep3.r
@@ -2,15 +2,15 @@
 ls()
 
 # we made a DTM for SJER. Now let's make one for HARV
-HARV_DTM <- rast("data/NEON-DS-Airborne-Remote-Sensing/HARV/DTM/HARV_dtmCrop.tif")
-HARV_DTM_hill <- rast("data/NEON-DS-Airborne-Remote-Sensing/HARV/DTM/HARV_DTMhill_WGS84.tif")
-HARV_DTM_df <- as.data.frame(HARV_DTM, xy = TRUE)
-HARV_DTM_hill_df <- as.data.frame(HARV_DTM_hill, xy = TRUE)
+DTM_HARV <- rast("data/NEON-DS-Airborne-Remote-Sensing/HARV/DTM/HARV_dtmCrop.tif")
+DTM_hill_HARV <- rast("data/NEON-DS-Airborne-Remote-Sensing/HARV/DTM/HARV_DTMhill_WGS84.tif")
+DTM_HARV_df <- as.data.frame(DTM_HARV, xy = TRUE)
+DTM_hill_HARV_df <- as.data.frame(DTM_hill_HARV, xy = TRUE)
 
 # this one is intentionally wrong
 # crazy stretch
 ggplot() +
-  geom_raster(data = HARV_DTM_df , 
+  geom_raster(data = DTM_HARV_df , 
               aes(x = x, y = y, 
                   fill = HARV_dtmCrop)) + 
   geom_raster(data = HARV_DTM_hill_df, 
@@ -21,14 +21,14 @@ ggplot() +
 
 # each one plots independently
 ggplot() +
-  geom_raster(data = HARV_DTM_df,
+  geom_raster(data = DTM_HARV_df,
               aes(x = x, y = y,
                   fill = HARV_dtmCrop)) +
   scale_fill_gradientn(name = "Elevation", colors = terrain.colors(10)) + 
   coord_quickmap()
 
 ggplot() +
-  geom_raster(data = HARV_DTM_hill_df,
+  geom_raster(data = DTM_hill_HARV_df,
               aes(x = x, y = y,
                   alpha = HARV_DTMhill_WGS84)) + 
   coord_quickmap()
@@ -37,32 +37,33 @@ ggplot() +
 ######################################
 # we could have figured this out first
 # (read the first line)
-crs(HARV_DTM, parse = TRUE)
-crs(HARV_DTM_hill, parse = TRUE)
+crs(DTM_HARV, parse = TRUE)
+crs(DTM_hill_HARV, parse = TRUE)
 
-(HARV_DTM)
-(HARV_DTM_hill)
+(DTM_HARV)
+(DTM_hill_HARV)
 
 
 # project can be a copy and paste operation
 # projectRaster(RasterObject, crs = CRSToReprojectTo)
 
-DTM_hill_UTMZ18N_HARV <- project(HARV_DTM_hill,crs(HARV_DTM))
+DTM_hill_UTMZ18N_HARV <- project(DTM_hill_HARV,
+                                 crs(DTM_HARV))
 
 
 crs(DTM_hill_UTMZ18N_HARV, parse = TRUE)
 
 #compare to before
-crs(HARV_DTM_hill, parse = TRUE)
+crs(DTM_hill_HARV, parse = TRUE)
 
 ext(DTM_hill_UTMZ18N_HARV)
-ext(HARV_DTM_hill)
+ext(DTM_hill_HARV)
 
 
 #Raster resolution:
 res(DTM_hill_UTMZ18N_HARV)
 
-res(HARV_DTM)
+res(DTM_HARV)
 
 # this still doesn't work because the EXTENTS don't match
 DTM_hill_UTMZ18N_df <- as.data.frame(DTM_hill_UTMZ18N_HARV, xy=TRUE)
@@ -79,9 +80,9 @@ ggplot() +
 
 #tell R to force newly reprojected raster to 1m x 1m resolution
 
-DTM_hill_UTMZ18N_HARV <- project(HARV_DTM_hill,
-                                 crs(HARV_DTM),
-                                 res = res(HARV_DTM))
+DTM_hill_UTMZ18N_HARV <- project(DTM_hill_HARV,
+                                 crs(DTM_HARV),
+                                 res = res(DTM_HARV))
 res(DTM_hill_UTMZ18N_HARV)
 
 
@@ -89,7 +90,7 @@ res(DTM_hill_UTMZ18N_HARV)
 DTM_hill_HARV_2_df <- as.data.frame(DTM_hill_UTMZ18N_HARV, xy = TRUE)
 
 ggplot() +
-  geom_raster(data = HARV_DTM_df , 
+  geom_raster(data = DTM_HARV_df , 
               aes(x = x, y = y, 
                   fill = HARV_dtmCrop)) + 
   geom_raster(data = DTM_hill_HARV_2_df, 
@@ -105,15 +106,16 @@ ggplot() +
 ##############################
 # challenge: make an overlay for SJER plotting digital terrain model
 # import DSM
-SJER_DSM <- rast("data/NEON-DS-Airborne-Remote-Sensing/SJER/DSM/SJER_dsmCrop.tif")
+DSM_SJER <- rast("data/NEON-DS-Airborne-Remote-Sensing/SJER/DSM/SJER_dsmCrop.tif")
 # import DSM hillshade
-SJER_DSM_hill_WGS <-
+DSM_hill_SJER_WGS <-
   rast("data/NEON-DS-Airborne-Remote-Sensing/SJER/DSM/SJER_DSMhill_WGS84.tif")
 
 # reproject raster
-SJER_DSM_hill_reprojected <- projectRaster(SJER_DSM_hill_WGS,
-                                           crs = crs(SJER_DSM),
-                                           res = 1)
+# This should be DSM not DTM right? Typo in challenge. 
+DSM_hill_UTMZ18N_SJER<- project(DSM_hill_SJER_WGS,
+                                crs(SJER_DSM),
+                                res = 1)
 
 # convert to data.frames
 SJER_DSM_df <- as.data.frame(SJER_DSM, xy = TRUE)
