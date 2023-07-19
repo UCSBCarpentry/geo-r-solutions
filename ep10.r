e793d5e0621339eafb49875a63c2e90e7311242e
kristi-sara <ksliu@ucsb.edu>
2023-07-18 17:14:11 -0700

    why is the naming for import and plot more points so different?

--- ep10.r
+++ ep10.r
@@ -6,52 +6,49 @@ library(terra)
 library(sf)
 
 #Import a new csv containing plot coordinates
-HARV_plot_locations <-
+plot_locations_HARV <-
   read.csv("data/NEON-DS-Site-Layout-Files/HARV/HARV_PlotLocations.csv")
-str(HARV_plot_locations)
-
-# Kristi note: make naming consistent, "HARV" should come first so we know the locale
+str(plot_locations_HARV)
 
 # check column names to look for spatial data
-names(HARV_plot_locations)
+names(plot_locations_HARV)
 
 # Easting/Northing contain fields containing spatial information 
-head(HARV_plot_locations$easting)
-head(HARV_plot_locations$northing)
+head(plot_locations_HARV$easting)
+head(plot_locations_HARV$northing)
 
 # Before we convert to a spatial object, we need to know the CRS 
 # when we ran line 11, we also have "geodeticDa" and "utmZone"
-head(HARV_plot_locations$geodeticDa)
-head(HARV_plot_locations$utmZone)
+head(plot_locations_HARV$geodeticDa)
+head(plot_locations_HARV$utmZone)
 
 # check crs of HARV points tower
-st_crs(HARV_points)
+st_crs(point_HARV)
 
 # Create CRS object that we can use to define the CRS of our new sf object when we create it
-utm18nCRS <- st_crs(HARV_points)
+utm18nCRS <- st_crs(point_HARV)
 utm18nCRS
 
 class(utm18nCRS)
 
 ## .csv to sf object 
 # specify the X Y coordinate values
-# specify the crs 
-
-HARV_plot_locations_sp <- st_as_sf(HARV_plot_locations, 
+# specify the crs
+plot_locations_sp_HARV <- st_as_sf(plot_locations_HARV, 
                                    coords = c("easting", "northing"),
                                    crs = utm18nCRS)
 # Check the new CRS to see if correct
-st_crs(HARV_plot_locations_sp)
+st_crs(plot_locations_sp-HARV)
 
 # Now plot the object 
 ggplot() +
-  geom_sf(data = HARV_plot_locations_sp) +
+  geom_sf(data = plot_locations_sp_HARV) +
   ggtitle("Map of Plot Locations")
 
 # Plot extent adding the aoi boundary 
 ggplot() +
   geom_sf(data = aoi_boundary_HARV) +
-  geom_sf(data = HARV_plot_locations_sp) +
+  geom_sf(data = plot_locations_sp_HARV) +
   ggtitle("AOI Boundary Plot")
 
 ## Challenge DIY import and plot additional points 
@@ -59,25 +56,26 @@ ggplot() +
 # 2. they are WGS84 
 # 3. plot the new points in the plot locations and add a different symbol for them
 
-newplot_loc_HARV <-
+newplot_locations_HARV <-
   read.csv("data/NEON-DS-Site-Layout-Files/HARV/HARV_2NewPhenPlots.csv")
-str(newplot_loc_HARV)
+str(newplot_locations_HARV)
 
 geogCRS <- st_crs(country_boundary_US)
 geogCRS
 
-Phenplot_sp_HARV <- st_as_sf(newplot_loc_HARV,
+#ok why is naming so different for this one?
+newPlot.Sp.HARV <- st_as_sf(newplot_loc_HARV,
                             coords = c("decimalLon", "decimalLat"),
                             crs = geogCRS)
-st_crs(Phenplot_sp_HARV)
+st_crs(newPlot.Sp.HARV)
 
 ggplot() +
-  geom_sf(data = HARV_plot_locations_sp, color = "orange") +
-  geom_sf(data = Phenplot_sp_HARV, color = "lightblue") +
+  geom_sf(data = plot_locations_sp_HARV, color = "orange") +
+  geom_sf(data = newPlot.Sp.HARV, color = "lightblue") +
   ggtitle("Map of All Plot Locations")
 
 ## Exporting an ESRI shapefile
-st_write(HARV_plot_locations_sp,
+st_write(plot_locations_sp_HARV,
          "data/HARV_PlotLocations.shp", driver = "ESRI Shapefile")
 
 
